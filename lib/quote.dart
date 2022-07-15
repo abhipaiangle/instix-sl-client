ad3a343f96bc178d594d4ab334b1c1fee10185b4
Code-Pulkit <pulkit.summers@gmail.com>
2022-07-15 21:00:41 +0530

    Learning

---
+++ lib/quote.dart
@@ -0,0 +1,7 @@
+class Quote {
+  String author;
+  String text;
+  Quote({required this.author, required this.text});
+}
+
+Quote myQuote = Quote(author: "Pulkit Jindal", text: "Hey How You doing");
